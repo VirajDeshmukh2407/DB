329c8da9938cee188757f763b8453f983022e490
viraj deshmukh <128155984+VirajDeshmukh2407@users.noreply.github.com>
2024-08-05 23:38:39 +0530

    files added

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,16 @@
 // src/firebase.js
+// require("dotenv").config();
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkL5QlYzbf_49qbgmTzTqGF9hyG50hZ2Q",
-  authDomain: "oauth-cc382.firebaseapp.com",
-  projectId: "oauth-cc382",
-  storageBucket: "oauth-cc382.appspot.com",
-  messagingSenderId: "175295182628",
-  appId: "1:175295182628:web:2d23330cd8757e0b89a5b1",
-  measurementId: "G-ZE446L6KBG",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
